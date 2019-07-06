f674ccb94e36f1a43a8acc3f9fdceb4f02dfd6dd
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-07-06 12:27:22 -0700

    merging functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -36,6 +36,7 @@ const config = {
 $('#push').on('click', function(){
   // console.log($('#name').val());
   const name = $('#name').val();
+  const desc = $('#desc').val();
   const date = $('#date').val();
   const type = $('#type').val();
   const street = $('#street').val();
@@ -46,6 +47,7 @@ $('#push').on('click', function(){
   // const lon = $('#lon').val();
   dataRef.ref().push({
     name: name,
+    desc: desc,
     date: date,
     type: type,
     street: street,
@@ -59,6 +61,14 @@ $('#push').on('click', function(){
 
 dataRef.ref().on('child_added', function(snapshot){
   // console.log(snapshot.val());
+  let name;
+  let date;
+  let description;
+  let game;
+  let lat;
+  let long;
+
+
   const queryString = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + snapshot.val().street + 
     "%2C%20" + snapshot.val().city + "%2C%20" + snapshot.val().state + "%20" + snapshot.val().zip + 
     ".json?access_token=pk.eyJ1Ijoiam9ucGtpbmciLCJhIjoiY2p4bW1kMjdsMDVkejNtcGF3azR6OWgyNSJ9.9PyL0KoB3385l1Se0xXz0g&cachebuster=1562436483413" + 
@@ -71,15 +81,26 @@ dataRef.ref().on('child_added', function(snapshot){
     method: "GET"
   }).then(function(response){
     console.log(response);
+    console.log(response.features[0].center);
+    lat = response.features[0].center[1];
+    long = response.features[0].center[0];
   });
+
+  name = snapshot.val().name;
+  date = snapshot.val().date;
+  description = snapshot.val().desc;
+  game = snapshot.val().type;
+
   const boardgameString = "https://www.boardgameatlas.com/api/search?name=" 
     + snapshot.val().type + "&client_id=SB1VGnDv7M";
   $.ajax({
     url: boardgameString,
     method: "GET"
   }).then(function(response){
-    console.log(response);
+    // console.log(response);
   })
+
+  // return {name: name, date: date, description: description, game: game, lat: lat, lon: long};
 });
 // let accessToken = getUrlParameter('code');
 
