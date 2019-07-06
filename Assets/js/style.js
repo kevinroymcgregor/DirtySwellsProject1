1a189badd4325e5327804c69388b0e0fe3dfa3c1
Diego Matthews <diegomatthews9@gmail.com>
2019-07-06 11:45:09 -0700

    Updated Carousel and redid color scheme

--- Assets/js/style.js
+++ Assets/js/style.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $('.carousel').carousel();
+
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
   });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
