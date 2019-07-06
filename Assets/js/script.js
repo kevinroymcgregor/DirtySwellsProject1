976e301985bad38f92db2d83faee85d3d08dac58
Enigma <jonpking@gmail.com>
2019-07-06 12:27:49 -0700

    updated code to use firebase references for data instead of using array of objects

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,34 @@
 const longitude = 51.505;
 const latitude = -0.09;
 const zoom = 13;
-const eventArray = [
-    {
-        name: "name1",
-        date: "date1",
-        description: "desc1",
-        game: "game2",
-        latitude: "lat1",
-        longitude: "long1"
-    },
-    {
-        name: "name2",
-        date: "date2",
-        description: "desc2",
-        game: "game2",
-        latitude: "lat2",
-        longitude: "long2"
-    },
-    {
-        name: "name3",
-        date: "date3",
-        description: "desc3",
-        game: "game3",
-        latitude: "lat3",
-        longitude: "long3"
-    }
-];
+// const eventArray = [
+//     {
+//         name: "name1",
+//         date: "date1",
+//         description: "desc1",
+//         game: "game2",
+//         latitude: "lat1",
+//         longitude: "long1"
+//     },
+//     {
+//         name: "name2",
+//         date: "date2",
+//         description: "desc2",
+//         game: "game2",
+//         latitude: "lat2",
+//         longitude: "long2"
+//     },
+//     {
+//         name: "name3",
+//         date: "date3",
+//         description: "desc3",
+//         game: "game3",
+//         latitude: "lat3",
+//         longitude: "long3"
+//     }
+// ];
+
+// TO DO: add pin functionality to map, add multiple pins so that each event drops a pin on the same map
 
 function createMap(longitude, latitude, zoom) {
     const mymap = L.map('mapDiv').setView([longitude, latitude], zoom);
@@ -41,35 +43,50 @@ function createMap(longitude, latitude, zoom) {
 // function for dynamic card generation
 function createEventCards() {
     $(".cardLocation").empty();
-    for (let i = 0; i < eventArray.length; i++) {
-        const col = $("<div class='col s3 m3'>");
-        const card = $("<div class='card-panel teal white-text'>");
-        card.append($("<span>").text(`Event Name: ${eventArray[i].name}`));
-        card.append("<hr>");
-        card.append("<span>" + "Event Date: " + eventArray[i].date + "</span>");
-        card.append("<br>");
-        card.append("<span>" + "Event Description: " + eventArray[i].description + "</span>");
-        card.append("<br>");
-        card.append("<span>" + "Game Type: " + eventArray[i].game + "</span>");
-        col.append(card)
-        $(".cardLocation").append(col);
-    }
+    const col = $("<div class='col s3 m3'>");
+    const card = $("<div class='card-panel teal white-text'>");
+    card.append($("<span>").text(`Event Name: ${name}`));
+    card.append("<hr>");
+    card.append("<span>" + "Event Date: " + date + "</span>");
+    card.append("<br>");
+    card.append("<span>" + "Event Description: " + description + "</span>");
+    card.append("<br>");
+    card.append("<span>" + "Game Type: " + game + "</span>");
+    col.append(card)
+    $(".cardLocation").append(col);
 }
 
+// function createEventCards() {
+//     $(".cardLocation").empty();
+//     for (let i = 0; i < eventArray.length; i++) {
+//         const col = $("<div class='col s3 m3'>");
+//         const card = $("<div class='card-panel teal white-text'>");
+//         card.append($("<span>").text(`Event Name: ${eventArray[i].name}`));
+//         card.append("<hr>");
+//         card.append("<span>" + "Event Date: " + eventArray[i].date + "</span>");
+//         card.append("<br>");
+//         card.append("<span>" + "Event Description: " + eventArray[i].description + "</span>");
+//         card.append("<br>");
+//         card.append("<span>" + "Game Type: " + eventArray[i].game + "</span>");
+//         col.append(card)
+//         $(".cardLocation").append(col);
+//     }
+// }
+
 // function to add new objects to eventArray using database data
-function addEvent(eventName, eventDate, eventDescription, eventGame, eventLatitude, eventLongitude) {
-    event.preventDefault();
-    const eventMeeting = {
-        name: eventName,
-        date: eventDate,
-        description: eventDescription,
-        game: eventGame,
-        latitude: eventLatitude,
-        longitude: eventLongitude
-    };
-    eventArray.push(eventMeeting);
-    createEventCards();
-}
+// function addEvent(eventName, eventDate, eventDescription, eventGame, eventLatitude, eventLongitude) {
+//     event.preventDefault();
+//     const eventMeeting = {
+//         name: eventName,
+//         date: eventDate,
+//         description: eventDescription,
+//         game: eventGame,
+//         latitude: eventLatitude,
+//         longitude: eventLongitude
+//     };
+//     eventArray.push(eventMeeting);
+//     createEventCards();
+// }
 
 // function addEvent() {
 //     event.preventDefault();
@@ -84,14 +101,6 @@ function addEvent(eventName, eventDate, eventDescription, eventGame, eventLatitu
 //     eventArray.push(eventMeeting);
 //     createEventCards();
 // }
-
-// function for search
-
-// function for filter
-
-// function to add data to database
-
-// function to pull data from database
 
 createMap(longitude, latitude, zoom);
 $(document).on("click", '#addEvent', addEvent);
